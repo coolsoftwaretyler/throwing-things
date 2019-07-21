16915c743134d80f0e84e81fd6778525b1cc9d97
Tyler Williams <tyler@ogdenstudios.xyz>
2019-07-21 14:48:24 -0600

    add shows and contact page, remov eunused readmes

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,11 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        throwingthings
-      </h1>
-      <h2 class="subtitle">
-        Throwing Things band website
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <iframe
+    style="border: 0; width: 350px; height: 470px;"
+    src="https://bandcamp.com/EmbeddedPlayer/album=3654087350/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
+    seamless
+  >
+    <a href="http://throwingthings.bandcamp.com/album/demos">
+      Demos by Throwing Things
+    </a>
+  </iframe>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>

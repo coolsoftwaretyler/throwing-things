16125c4660704366eb7c41ce9c7717154440b279
Tyler Williams <tyler@ogdenstudios.xyz>
2019-07-21 15:39:56 -0600

    update index with badncamp bruno demo album

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <iframe
-    style="border: 0; width: 350px; height: 470px;"
-    src="https://bandcamp.com/EmbeddedPlayer/album=3654087350/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/"
+    style="border: 0; width: 350px; height: 786px;"
+    src="https://bandcamp.com/EmbeddedPlayer/album=3807523161/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
     seamless
   >
-    <a href="http://throwingthings.bandcamp.com/album/demos">
-      Demos by Throwing Things
-    </a>
+    <a
+      href="http://throwingthings.bandcamp.com/album/the-bruno-demos"
+    >The Bruno Demos by Throwing Things</a>
   </iframe>
 </template>

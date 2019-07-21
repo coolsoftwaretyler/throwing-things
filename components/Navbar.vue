738babdc13c71802a9464cd1c3589fc9675c21b2
Tyler Williams <tyler@ogdenstudios.xyz>
2019-07-21 15:46:12 -0600

    add navbar component to default layout

---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <ul>
+      <li><nuxt-link to="/">Home</nuxt-link></li>
+      <li><nuxt-link to="/shows">Shows</nuxt-link></li>
+      <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+    </ul>
+  </nav>
+</template>

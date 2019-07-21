dfd32669ac729556503709261be2fe6a23b939e9
Tyler Williams <tyler@ogdenstudios.xyz>
2019-07-21 15:52:44 -0600

    add some padding and layout and stuff to the home page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,34 @@
 <template>
   <nav>
     <ul>
-      <li><nuxt-link to="/">Home</nuxt-link></li>
-      <li><nuxt-link to="/shows">Shows</nuxt-link></li>
-      <li><nuxt-link to="/contact">Contact</nuxt-link></li>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/shows">Shows</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/contact">Contact</nuxt-link>
+      </li>
     </ul>
   </nav>
 </template>
 
 <style lang="scss" scoped>
 nav {
-  display: flex;
+  padding: 1em;
+  width: 100%;
+  ul {
+    display: flex;
+    list-style: none;
+    margin-left: 0;
+    padding-left: 0;
+    width: 100%;
+    li {
+      margin-left: 0;
+      margin-right: 2em;
+      padding: 2em 0;
+    }
+  }
 }
 </style>

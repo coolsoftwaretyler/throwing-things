9b0049480ad481f2baa07836193a848ac7bbb375
Tyler Williams <tyler@ogdenstudios.xyz>
2019-07-21 19:42:33 -0600

    add contact content

---
+++ pages/contact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Find us on:</p>
+    <ul>
+      <li v-for="item in social" :key="item.name">
+        <a :href="item.link">{{ item.name }}</a>
+      </li>
+    </ul>
+    <p>
+      Or
+      <a href="mailto:throwingthingsandstuff@gmail.com">email us</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+import social from "~/assets/json/social.json";
+export default {
+  data: function() {
+    return {
+      social: social
+    };
+  }
+};
+</script>

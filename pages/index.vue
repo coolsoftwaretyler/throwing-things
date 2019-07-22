70e22605be0b6b521bb4ff9288465a089335c6e0
Tyler Williams <tyler@ogdenstudios.xyz>
2019-07-21 19:04:05 -0600

    add some formatting to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,44 @@
 <template>
-<div>
-
-<div v-html="copy"></div>
-  <iframe
-    style="border: 0; width: 350px; height: 786px; max-width: 100%;"
-    src="https://bandcamp.com/EmbeddedPlayer/album=3807523161/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
-    seamless
-  >
-    <a
-      href="http://throwingthings.bandcamp.com/album/the-bruno-demos"
-    >The Bruno Demos by Throwing Things</a>
-  </iframe>
+  <div class="container">
+    <div class="intro" v-html="copy"></div>
+    <iframe
+      style="border: 0; width: 350px; height: 786px; margin: 2em auto; max-width: 100%;"
+      src="https://bandcamp.com/EmbeddedPlayer/album=3807523161/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/"
+      seamless
+    >
+      <a
+        href="http://throwingthings.bandcamp.com/album/the-bruno-demos"
+      >The Bruno Demos by Throwing Things</a>
+    </iframe>
   </div>
-
 </template>
 
 <script>
-import copy from "~/assets/markdown/index.md"
+import copy from "~/assets/markdown/index.md";
 
 export default {
-  data: function () {
+  data: function() {
     return {
       copy: copy
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  max-width: 650px;
+}
+.intro {
+  ul {
+    list-style: none;
+    margin-left: 0;
+    margin-top: 2em;
+    padding-left: 0;
+    li {
+      margin-left: 0;
     }
   }
 }
-</script>
+</style>
